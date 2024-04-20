ada5b9b5ff72b7005dc182ee6a543807af2c3b67
dibya.borah <dibyajyotiborah469@gmail.com>
2024-04-20 22:06:14 +0530

    Removed consoles

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -5,7 +5,6 @@ import { useLocation } from "react-router-dom";
 const RequireAuth = ({ children }) => {
   const { isLoggedIn, setIsLoggedIn } = useAuth();
   const location = useLocation();
-  console.log('RequireAuth', location);
 
   if (isLoggedIn) {
     return children;
