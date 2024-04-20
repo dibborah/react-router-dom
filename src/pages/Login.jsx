c8bcc3aec150ff2e76f5c5151cbca098ccd5d801
dibya.borah <dibyajyotiborah469@gmail.com>
2024-04-20 21:53:19 +0530

    using useLocation and its state and pathname property to navigate back to the routes it came from

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthProvider";
+import { useLocation } from "react-router-dom";
 
 const Login = () => {
   const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const location = useLocation();
+  const previousPath = location.state?.previousPath || "/";
+
   const navigate = useNavigate();
   const handleLogin = () => {
     setIsLoggedIn(true);
-    navigate("/", { replace: true });
+    navigate(previousPath, { replace: true }); // here it is not a react component or a html tag
+    // So just writing the property , for ex. : replace won't make the value to be true by default
+    // Once has to explicitly write true if you want the value to be true
   };
   return (
     <div>
